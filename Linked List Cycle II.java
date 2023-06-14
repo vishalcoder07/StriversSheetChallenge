e357d2b255154dab1773ab673d02ac7e1e9cb296
Vishal Gaikwad <vishalcoder07@gmail.com>
2023-06-14 20:07:25 +0530

    Create Linked List Cycle II.java

---
+++ Linked List Cycle II.java
@@ -0,0 +1,52 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution {
+    public static Node firstNode(Node head) {
+        if(head == null) return null;
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                Node cur = head;
+                
+                while(cur != slow){
+                    cur = cur.next;
+                    slow = slow.next;
+                }
+                return cur;
+            }
+        }
+
+        return null;
+    }
+}
