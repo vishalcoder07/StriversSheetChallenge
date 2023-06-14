2646467da84307ed0993b4c3f7ce150835737971
Vishal Gaikwad <vishalcoder07@gmail.com>
2023-06-14 19:15:14 +0530

    Update Cycle Detection in a Singly Linked List.java

--- Cycle Detection in a Singly Linked List.java
+++ Cycle Detection in a Singly Linked List.java
@@ -1,1 +1,45 @@
+/**************************************************************
 
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution {
+
+    public static boolean detectCycle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) return true;
+        }
+
+        return false;
+    }
+}
